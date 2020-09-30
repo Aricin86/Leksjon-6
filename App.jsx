7874026e5e3cf201fb9cf841e12e91a514cc1fe3
Åse Ulriksen Rørmark <43664307+Aricin86@users.noreply.github.com>
2020-09-30 12:33:41 +0200

    A working example of React and API from brreg. Assignemnt remodelling starts here.

--- App.jsx
+++ App.jsx
@@ -1,53 +1,39 @@
-// import React, { useState } from 'react';
-// import Title from './src/components/Title';
-// import Search from './src/components/Search';
-// import List from './src/components/List';
+import React, { useState } from 'react';
+import Title from './src/components/Title';
+import Search from './src/components/Search';
+import List from './src/components/List';
 
-// const URL =
-//   'https://data.brreg.no/enhetsregisteret/api/enheter?konkurs=false&organisasjonsnummer=';
+const URL =
+  'https://data.brreg.no/enhetsregisteret/api/enheter?konkurs=false&organisasjonsnummer=';
 
-// const greeting = 'Velkommen til Demo';
+const greeting = 'Velkommen til Demo';
 
-// const App = () => {
-// const [search, setSearch] = useState('');
-// const [data, setData] = useState('');
+const App = () => {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState('');
 
-// const fetchData = async () => {
-//   const response = await fetch(URL + search);
-//   const companyData = await response.json();
-//   setData(companyData._embedded.enheter);
-// };
+  // fetching data
+  const fetchData = async () => {
+    const response = await fetch(URL + search);
+    const companyData = await response.json();
+    setData(companyData._embedded.enheter);
+  };
 
-// const handleSearch = (e) => {
-//   setSearch(e.target.value);
-// };
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-// return (
-// <>
-//   <Title title={greeting} />
-//   <Search handleSearch={handleSearch} search={search} />
-//   <button onClick={fetchData} type="button">
-//     Søk
-//   </button>
-//   <hr />
-//   <List list={data} />
-// </>
-
-// );
-// };
-
-import React from 'react';
-
-const App = () => (
-  <>
-    <h1>Some title </h1>
-    <form id="search">
-      <label htmlFor="search">Søk: </label>
-      <input type="text" id="search" placeholder="org.nr" />
-      <button type="submit">Finn selskap</button>
-    </form>
-    <hr />
-  </>
-);
+  return (
+    <>
+      <Title title={greeting} />
+      <Search handleSearch={handleSearch} search={search} />
+      <button onClick={fetchData} type="button">
+        Søk
+      </button>
+      <hr />
+      <List list={data} />
+    </>
+  );
+};
 
 export default App;
